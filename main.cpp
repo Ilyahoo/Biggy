f1acb20cba9e3e27b49d57e27a2cef0417b54e8e
unknown <irens@live.nl>
2017-12-21 21:01:57 +0000

    Testing remote

--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 using namespace std;
 
 int main()
-{   BigInt a1;
-    cin >> a1;
-    vector<int> lets_see;
-    lets_see = a1.GetNumber();
-    for(vector<int>::reverse_iterator it = lets_see.rbegin(); it != lets_see.rend(); it++){
-        cout << *it;
-    }
-    cout << "Hello world!" << endl;
+{   
+	BigInt a1;
+	cin >> a1;
+
+    
     return 0;
 }
