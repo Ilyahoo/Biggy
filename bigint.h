d19c74ab53a61ab96af6ec93b0f5aade2ebe7d8d
unknown <irens@live.nl>
2018-01-03 22:42:56 +0000

    Something

--- bigint.h
+++ bigint.h
@@ -13,32 +13,49 @@ class BigInt
 {
     friend istream& operator>>(istream& In,  BigInt& Passed);
     friend ostream& operator<<(ostream& Out, BigInt& Passed);
+//   friend Argements; //bach azayas nissi Argements
+//friend Argements Biggy;
 
     public:
-        BigInt();//notice to overload this with different type and dont forgot to set len and sign
+        BigInt();/**notice to overload this with few different type and dont forget to set len and sign**/
+        BigInt(string init);
+        BigInt(vector<int> init);
         void FromString(string& From);
         void FromString(string& From, bool Rev);
         void Del_Fst_Zeros(string& From, bool Rev);
         void Set_Sign(string& From);
         void Set_Sign(bool What);
+        void Sub_Helper(vector<int>& nm1, vector<int>& nm2);
+        void FromVector_int(vector<int>& Passed);
+        void Del_Fst_Zeros(string& From);
+        void SetNumber(vector<int> val) { Number = val; }
+        void SetN_Len(unsigned int val) { N_Len = val; }
 
-	string ToString();
-	void Del_Fst_Zeros(string& From);
 
-	void FromVector_int(vector<int>& Passed);
-	BigInt operator+(BigInt& rhs);
-	BigInt operator*(BigInt& rhs);
-	BigInt operator-();
+
+
+        BigInt operator+(BigInt& rhs);
+        BigInt operator-(BigInt& rhs);
+        BigInt operator*(BigInt& rhs);
+        BigInt operator-();
  /**   BigInt operator=(BigInt& rhs);**/
 
         bool Get_Sign() { return G_Sign; }
-
+        string ToString();
         vector<int> GetNumber() { return Number; }
-        void SetNumber(vector<int> val) { Number = val; }
         unsigned int GetN_Len() { return N_Len; }
-        void SetN_Len(unsigned int val) { N_Len = val; }
 
-    protected:
+
+        bool operator<(BigInt& rhs);
+        bool operator>(BigInt& rhs);
+        bool operator<=(BigInt& rhs);
+        bool operator>=(BigInt& rhs);
+        bool operator==(BigInt& rhs);
+        bool operator!=(BigInt& rhs);
+
+
+
+
     private:
         bool G_Sign;
         vector<int> Number;
